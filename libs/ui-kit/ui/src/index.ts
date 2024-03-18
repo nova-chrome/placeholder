b1de4d16875c974166b28fb77407d84958049f19
Victor Avila <victoravilamorales95@gmail.com>
2024-03-18 11:04:58 -0700

    feat: add more components

--- libs/ui-kit/ui/src/index.ts
+++ libs/ui-kit/ui/src/index.ts
@@ -1,4 +1,7 @@
 export * from './lib/button';
+export * from './lib/checkbox';
+export * from './lib/dropdown-menu';
+export * from './lib/input';
 export * from './lib/sheet';
 export * from './lib/table';
 export * from './lib/theme-toggle';
