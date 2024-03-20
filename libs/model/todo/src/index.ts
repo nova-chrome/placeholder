3a9aaba299f49ac4baab521f7cf8129a1d89d6c6
Victor Avila <victoravilamorales95@gmail.com>
2024-03-19 17:42:26 -0700

    feat: update repo model

--- libs/model/todo/src/index.ts
+++ libs/model/todo/src/index.ts
@@ -1,2 +1,8 @@
 export { Todo } from './lib/todo.model';
-export { TODO_REPOSITORY_TOKEN, ITodoRepository } from './lib/todo.repository';
+export {
+  FindAllTodosRequestParams,
+  FindAllTodosRequestApiParams,
+  FindAllResponse,
+  TODO_REPOSITORY_TOKEN,
+  ITodoRepository,
+} from './lib/todo.repository';
--- libs/model/todo/src/lib/todo.repository.ts
+++ libs/model/todo/src/lib/todo.repository.ts
@@ -2,6 +2,21 @@ import { Todo } from './todo.model';
 
 export const TODO_REPOSITORY_TOKEN = 'TODO_REPOSITORY_TOKEN';
 
+export interface FindAllTodosRequestParams {
+  pageIndex: number;
+  pageSize: number;
+}
+
+export interface FindAllTodosRequestApiParams {
+  _page: number;
+  _limit: number;
+}
+
+export interface FindAllResponse {
+  data: Todo[];
+  total: number;
+}
+
 export interface ITodoRepository {
-  findAll: () => Promise<Todo[]>;
+  findAll: (params: FindAllTodosRequestParams) => Promise<FindAllResponse>;
 }
