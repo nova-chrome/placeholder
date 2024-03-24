2330a87a7f6640c550ae647cc48e618116a8ccb1
Victor Avila <victoravilamorales95@gmail.com>
2024-03-23 21:58:32 -0700

    fix: status issue on done checkboxes

--- libs/todo/feature/todos/src/lib/columns.tsx
+++ libs/todo/feature/todos/src/lib/columns.tsx
@@ -6,9 +6,8 @@ import {
   TruncateTooltip,
 } from '@placeholder/ui-kit/ui';
 import { cn } from '@placeholder/ui-kit/util';
-import { ColumnDef, Row } from '@tanstack/react-table';
+import { ColumnDef } from '@tanstack/react-table';
 import { X } from 'lucide-react';
-import React from 'react';
 
 export const COLUMNS: ColumnDef<Todo>[] = [
   {
@@ -21,20 +20,28 @@ export const COLUMNS: ColumnDef<Todo>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Title" />
     ),
-    cell: ({ row }) => (
-      <TruncateTooltip
-        className={cn(
-          'max-w-[400px]',
-          row.original.completed && 'line-through'
-        )}
-        text={row.original.title}
-      />
-    ),
+    cell: ({ row }) => {
+      return (
+        <TruncateTooltip
+          className={cn(
+            'max-w-[400px]',
+            row.original.completed && 'line-through'
+          )}
+          text={row.original.title}
+        />
+      );
+    },
   },
   {
     accessorKey: 'completed',
     header: () => <div className="text-center">Done</div>,
-    cell: ({ row }) => <Completed row={row} />,
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-center text-center">
+          <Checkbox checked={row.original.completed} className="pr-0" />
+        </div>
+      );
+    },
   },
   {
     accessorKey: '',
@@ -48,18 +55,3 @@ export const COLUMNS: ColumnDef<Todo>[] = [
     },
   },
 ];
-
-function Completed({ row }: { row: Row<Todo> }) {
-  const completed = row.original.completed;
-  const [isCompleted, setIsCompleted] = React.useState(completed);
-
-  return (
-    <div className="flex justify-center text-center">
-      <Checkbox
-        checked={isCompleted}
-        onCheckedChange={() => setIsCompleted((prev) => !prev)}
-        className="pr-0"
-      />
-    </div>
-  );
-}
